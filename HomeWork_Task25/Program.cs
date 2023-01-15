eacb8813966f99c8d05f3dd25d362b799d441b07
Khursan Lyudmila <khursanlyudmila@gmail.com>
2023-01-15 13:45:26 +0300

    add Home Work Task 25

---
+++ HomeWork_Task25/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или Ваше число меньше 1!");
+        }
+    }
+
+    return result;
+}
+
+int number1 = GetNumber("Введите число 1: ");
+int number2 = GetNumber("Введите число 2: ");
+int result = 1;
+{
+    for (int i = 1; i <= number2; i++)
+    {
+         result = result * number1;
+    }
+    Console.WriteLine($"Число {number1} в степени {number2} равно {result}");
+    return result;
+}
+
