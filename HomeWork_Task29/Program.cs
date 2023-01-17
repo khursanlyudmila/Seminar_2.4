22d1fdd5595497fe3bf9aae08885871c93069725
Khursan Lyudmila <khursanlyudmila@gmail.com>
2023-01-17 14:10:03 +0300

    add Home Work Task 29

---
+++ HomeWork_Task29/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+
+все 8 элементов должны получить за один Console.ReadLine();
+подсказка: использовать потом метод string.Split();
+*/
+
+Console.WriteLine("Необходимо ввести массив из 8 элементов, например: 1, 2, 3, 4, 5, 6, 7, 8");
+Console.WriteLine("Введите массив: ");
+
+String str = Console.ReadLine();
+  
+String[] spearator = {", ", ". "};
+Int32 count = 8;
+  
+String[] strlist = str.Split(spearator, count,
+    StringSplitOptions.RemoveEmptyEntries);
+  
+foreach(String s in strlist)
+{
+    Console.WriteLine(s);
+}
+
+
